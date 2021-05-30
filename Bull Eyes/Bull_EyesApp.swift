5c4ff326b885b6f557f8e84ecbdb2d365b055c57
Giorgio Giannotta <jorge_upa@msn.com>
2021-05-30 15:34:12 +0200

    Initial Commit

---
+++ Bull Eyes/Bull_EyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bull_EyesApp.swift
+//  Bull Eyes
+//
+//  Created by Jorge Giannotta on 30/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Bull_EyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
